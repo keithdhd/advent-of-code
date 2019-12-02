9de0fb43f6f75aaabffebd882fb0e5559f72fe5a
Valerie Dryden <outragedpinkracoon@gmail.com>
2019-12-02 22:20:33 +0000

    Code runs with unit tests

--- spec/opcode_spec.rb
+++ spec/opcode_spec.rb
@@ -3,7 +3,6 @@
 require_relative '../day2.rb'
 
 RSpec.describe Day2 do
-
   describe '#self.test' do
     it 'adds puts string to screen' do
       expect(Day2.test).to eql("hey-yo! This is day 2 ya'll")
@@ -12,17 +11,10 @@ RSpec.describe Day2 do
 
   describe '#self.run' do
     it 'adds puts to screen' do
-      expect(Day2.run([1,0,0,0,99])).to eql([2,0,0,0,99])
-      expect(Day2.run([2,3,0,3,99])).to eql([2,3,0,6,99])
-      expect(Day2.run([2,4,4,5,99,0])).to eql([2,4,4,5,99,9801])
-      expect(Day2.run([1,1,1,4,99,5,6,0,99])).to eql([30,1,1,4,2,5,6,0,99])
+      expect(Day2.run([1, 0, 0, 0, 99])).to eql([2, 0, 0, 0, 99])
+      expect(Day2.run([2, 3, 0, 3, 99])).to eql([2, 3, 0, 6, 99])
+      expect(Day2.run([2, 4, 4, 5, 99, 0])).to eql([2, 4, 4, 5, 99, 9801])
+      expect(Day2.run([1, 1, 1, 4, 99, 5, 6, 0, 99])).to eql([30, 1, 1, 4, 2, 5, 6, 0, 99])
     end
   end
-  
-  describe '#self.awesome_array' do
-    it 'adds puts to screen' do
-      expect(Day2.awesome_array([1,0,34,0,99])).to eql(34)
-    end
-  end
-
 end
