9fc13444d53847693f386775aa106c75564be1b4
Gideon Brimleaf <chrisdryden1@gmail.com>
2019-12-03 21:57:36 +0000

    we're making the ship an object

--- day1.rb
+++ day1.rb
@@ -3,8 +3,16 @@
 #This program runs a thing
 class Day1
 
-  def self.fuel_rounder(module_mass) 
-    2
+  def initialize(fuel_modules)
+    @fuel_modules = fuel_modules
+  end
+
+  def fuel_calculator(modules)
+
+  end
+
+  def fuel_per_module(module_mass) 
+    module_mass / 3 - 2
   end
 
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -4,12 +4,13 @@ require_relative '../day1.rb'
 
 RSpec.describe Day1 do
 
-  describe '.self.fuel_rounder' do
-    it 'rounds down and subtracts 2' do
-      expect(Day1.fuel_rounder(12)).to eql(2)
-      expect(Day1.fuel_rounder(14)).to eql(2)
-      expect(Day1.fuel_rounder(1969)).to eql(654)
-      # expect(Day1.fuel_rounder(100756)).to eql(33583)
+  describe '.self.fuel_per_module' do
+    it '#rounds down and subtracts 2' do
+      ship = Day1.new([1,2,3,4])
+      expect(ship.fuel_per_module(12)).to eql(2)
+      expect(ship.fuel_per_module(14)).to eql(2)
+      expect(ship.fuel_per_module(1969)).to eql(654)
+      expect(ship.fuel_per_module(100756)).to eql(33583)
     end
   end
 end
