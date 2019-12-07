04bb29c71ee1cde8d17ac1fb47cf5cff5fdb2c8c
Gideon Brimleaf <chrisdryden1@gmail.com>
2019-12-07 17:15:06 +0000

    initial attempt

---
+++ day6.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# This does an orbit
+module Day3
+  def self.orbit_count(planets)
+    planets.count-1
+  end
+
+  def self.orbit_builder(planets)
+    #needs to returb {a => b, b=> c, c=> e}
+    orbits = {}
+    orbits[planets[0]] = 0
+    planets[1..].each do |planet|
+      orbits[planet] = 1
+    end
+    orbits
+  end
+
+end
---
+++ spec/day6_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../day6.rb'
+
+RSpec.describe Day6 do
+
+  describe '.orbit_count' do
+    it 'counts the number of orbits' do
+      expect(Day6.orbit_count(['a','b'])).to eq(1)
+    end
+  end 
+
+  describe '.orbit_builder' do
+    it 'describes orbits for planets' do
+      expect(Day6.orbit_builder(['a','b'])).to eq({'a'=>0, 'b'=>1})
+    end
+  end 
+
+end
