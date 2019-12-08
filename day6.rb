5f4e4d1662731a3aeba7d251dd248f7ed9c0a9a0
Gideon Brimleaf <chrisdryden1@gmail.com>
2019-12-08 17:19:33 +0000

    can count the number of direct/indirect orbits

--- day6.rb
+++ day6.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 # This does an orbit
-module Day3
-  def self.orbit_count(planets)
-    planets.count-1
+module Day6
+  def self.orbit_count(orbits)
+    total = 0
+    orbits.each do |orbit, planet|
+      total += planet.count
+    end
+    total
   end
 
   def self.orbit_builder(planets)
-    #needs to returb {a => b, b=> c, c=>e}
-    orbits = {}
-    orbits[planets[0]] = 0
-    planets[1..].each do |planet|
-      orbits[planet] = 1
+    #needs to return {a => b, b=> c, c=>e}
+    planets[1..].each_with_object({}) do |planet, orbits|
+      orbits[planet] = planet[]
     end
-    orbits
   end
 
 end
--- spec/day6_spec.rb
+++ spec/day6_spec.rb
@@ -6,15 +6,18 @@ RSpec.describe Day6 do
 
   describe '.orbit_count' do
     it 'counts the number of orbits' do
-      expect(Day6.orbit_count(['a','b'])).to eq(1)
+      expect(Day6.orbit_count({'b'=>['a']})).to eq(1)
+      expect(Day6.orbit_count({'g'=>['b','a'], 'h'=>['g','b','a']})).to eq(5)
     end
   end 
 
-  describe '.orbit_builder' do
-    it 'describes orbits for planets' do
-      expect(Day6.orbit_builder(['a','b'])).to eq({'a'=>0, 'b'=>1})
-      expect(Day6.orbit_builder(['a','b'])).to eq({'a'=>0, 'b'=>1})
-    end
-  end 
+  # describe '.orbit_builder' do
+  #   it 'describes orbits for planets' do
+  #     expect(Day6.orbit_builder(['a','b'])).to eq({'a'=> 'b'})
+  #   end
+  # end 
 
 end
+
+# {a=>b, b=>c, b=>g , g=>h, c=>d}
+# {b=>[a], c =>[b,a], d=>[c,b,a], g=>[b,a], h=>[g,b,a]}
